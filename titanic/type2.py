2cea85b179fddf813f8560b61190fd12e9f755a8
Sleepincoffee <cs781503542@qq.com>
2018-04-21 17:21:50 +0800

    完成deeplearing.ai 第一课第二周作业

--- titanic/type2.py
+++ titanic/type2.py
@@ -17,22 +17,4 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import learning_curve
 
-path = r'C:\Users\chenshuai\Documents\Kaggle\Titanic\\'
-train = pd.read_csv(path + 'train.csv')
-test = pd.read_csv(path + 'test.csv')
-
-from scipy import stats
-fig, axes = plt.subplots(2,1,figsize=(8,6))
-sns.set_style('white')
-sns.distplot(train.Age.fillna(-20), rug=True, color='b', ax=axes[0])
-ax0 = axes[0]
-ax0.set_title('age distribution')
-ax0.set_xlabel('')
-
-ax1 = axes[1]
-ax1.set_title('age survived distribution')
-k1 = sns.distplot(train[train.Survived==0].Age.fillna(-20), hist=False, color='r', ax=ax1, label='dead')
-k2 = sns.distplot(train[train.Survived==1].Age.fillna(-20), hist=False, color='g', ax=ax1, label='alive')
-ax1.set_xlabel('')
-
-ax1.legend(fontsize=16)
+#完成deeplearing.ai 第一课第二周作业
