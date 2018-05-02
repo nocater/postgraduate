38ff9271e6b1a4dbd05b9812a731df4023f54921
Sleepincoffee <cs781503542@qq.com>
2018-05-02 11:41:21 +0800

    完成贝氏体板条宽度W拟合公式

---
+++ material/temp_work.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import numpy as np
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+df = pd.read_excel(r'C:\Users\chenshuai\Documents\材料学院\贝氏体板条宽度W公式拟合.xlsx')
+print(df.columns)
+df = df.rename(columns=dict(zip(df.columns,['C','T','theta','G','W','from','other'])))
+df = df[['C','T','theta','G','W']]
+df.loc[:, 'W'] = [x*1000 for x in df.loc[:, 'W']]
+
+x = df.iloc[:, :-1]
+y = df.W
+
+lr = LinearRegression()
+lr.fit(x,y)
+print(f'{lr.score(x, y):.2}')
+y_pred = lr.predict(x)
+print(f'MAE:{int(mean_absolute_error(y, y_pred))}')
+print(f'MSE:{int(mean_squared_error(y, y_pred))}')
+print(f'RMSE:{int(np.sqrt(mean_squared_error(y, y_pred)))}')
+
+parameters = pd.DataFrame(np.append(np.array(lr.coef_), [lr.intercept_]), index=['C','T','σγ','ΔGγ','intercept'])
+print(parameters)
