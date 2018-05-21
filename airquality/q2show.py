10550dd546d2c742486d0da022f9aadbc834cef1
Sleepincoffee <cs781503542@qq.com>
2018-05-21 23:14:00 +0800

    添加Q2_xt数据显示

--- airquality/q2show.py
+++ airquality/q2show.py
@@ -9,18 +9,18 @@ if __name__=='__main__':
     石家庄全年数据预测模型
     测试集合108
     """
-    df = pd.read_csv(r'C:\Users\chenshuai\Documents\airquality\result\result0.csv')
+    df = pd.read_csv(r'C:\Users\chenshuai\Documents\airquality\result\Question2_xt.csv')
     df = shuffle(df, random_state=3)
     y1 = df.predict.values
     y2 = df.real.values
 
     # y1, y2 = shuffle(y1, y2, random_state=3)
-    x = range(len(y1))
-    plt.plot(x, y1, label='pridection')  # 'o-',
-    plt.plot(x, y2, label='real')        # '*-',
+    x = range(1, len(y1)+1)
+    plt.plot(x, y1, '-', label='pridection')  # 'o-',
+    plt.plot(x, y2, '-.', label='real')       # '*-',
     plt.legend()
-    plt.xlabel('TestSet')
-    plt.ylabel('pm2.5')
-    plt.title('SJZ_RFModel')
+    plt.xlabel('test')
+    plt.ylabel('PM2.5')
+    plt.title('XT_RFModel')
     plt.show()
     #  R2:0.82/0.69  MAE:34  MSE:1946  RMSE:44
