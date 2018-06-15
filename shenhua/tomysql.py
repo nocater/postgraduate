781ddfa533a5da5004cd9c3eae39c240b022eddf
nocater <nocater@163.com>
2018-06-15 13:13:17 +0800

    完成对流程定义表分析翻车机到生产部门的关联分析

--- shenhua/tomysql.py
+++ shenhua/tomysql.py
@@ -1,27 +1,46 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import mysql.connector
+import re
 
-PATH = r'C:\Users\chenshuai\Documents\神华黄骅\文档\\'
+PATH = r'C:\Users\chenshuai\Documents\神华黄骅\数据\\'
 
-# workflow data
-workflow = pd.read_excel(PATH+'workflow.xlsx')
-workflow.rename(columns={'   ':'ID'}, inplace=True)
-# chage the value type
-# workflow.FID = workflow.FID.astype('int64')
-# date
-# workflow.WORKDATE = pd.to_datetime(workflow.WORKDATE)
-for col in ['WORKDATE', 'STARTTIME', 'ENDTIME', 'MODIFYTIME', 'CONFIRMTIME']:
-    workflow[col] = pd.to_datetime(workflow[col])
+def analysisCD_S():
+    # 列出工作流程定义中 翻车机和堆料机的对应
+    cd_s = []
+    df = pd.read_excel(PATH + '作业流程定义.xls')
+    for cd in range(1, 14):
+        for i in list(df.设备串):
+            if re.match(r'CD\d+.*S[/R\d?|\d+]', i):
+                cd_s.append(str(i.split('-')[0]+'-'+i.split('-')[-1]))
+                # print(i, i.split('-')[-1])
 
-print(workflow.columns)
-workflow.info()
+    cd_s = sorted(list(set(cd_s)))
+    print(cd_s)
+    df = pd.DataFrame(cd_s)
+    df.to_csv(PATH+'CD_S.csv')
 
-# planload data
-# planload = pd.read_excel(PATH+'planload.xlsx')
-# planload.rename(columns={'   ':'id'}, inplace=True)
+def exceltomysql():
+    # workflow data
+    workflow = pd.read_excel(PATH+'workflow.xlsx')
+    workflow.rename(columns={'   ':'ID'}, inplace=True)
+    # chage the value type
+    # workflow.FID = workflow.FID.astype('int64')
+    # date
+    # workflow.WORKDATE = pd.to_datetime(workflow.WORKDATE)
+    for col in ['WORKDATE', 'STARTTIME', 'ENDTIME', 'MODIFYTIME', 'CONFIRMTIME']:
+        workflow[col] = pd.to_datetime(workflow[col])
 
-# create_engine("数据库类型+数据库驱动://数据库用户名:数据库密码@IP地址:端口/数据库"，其他参数)
-engine=create_engine("mysql+pymysql://root:2wsx3edc@localhost:3306/shenhua?charset=utf8", echo=True)
-workflow.to_sql('workflow', con=engine, index=False)
-planload.to_sql('planload', con=engine, index=False)
+    print(workflow.columns)
+    workflow.info()
+
+    # create_engine("数据库类型+数据库驱动://数据库用户名:数据库密码@IP地址:端口/数据库"，其他参数)
+    engine=create_engine("mysql+pymysql://root:2wsx3edc@localhost:3306/shenhua?charset=utf8", echo=True)
+    workflow.to_sql('workflow', con=engine, index=False)
+
+    # planload data
+    # planload = pd.read_excel(PATH+'planload.xlsx')
+    # planload.rename(columns={'   ':'id'}, inplace=True)
+
+if __name__ == '__main__':
+    analysisCD_S()
