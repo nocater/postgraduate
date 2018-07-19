bc94e46f2d1dfafe25f9c9eaad173b74c7654c3d
nocater <nocater@163.com>
2018-07-19 11:04:20 +0800

    改用MinMaxScaler数据缩放

--- material/datahelper.py
+++ material/datahelper.py
@@ -1,10 +1,3 @@
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-
-
 class feature_energing:
     """
         数据处理
@@ -19,23 +12,24 @@ class feature_energing:
         X:自变量df
         Y:因变量df
     """
+
     def __init__(self, file,
-                    columns:'选取的数据维度' = ['C', 'Si', 'Mn', 'Ni', 'Cr', 'Mo', 'Al', 'Co', '等温温度T2', '回火温度T3', '抗拉强度'],
-                    scaler:'是否采用数据缩放' = StandardScaler,
-                    regression:'用于分类还是回归'= True,
-                    shuffle:''= True,
-                    random_seed:''= 1,
-                    categories:'分类类别' = 4,
-                    onehot:'是否使用onehot'= False,
-                    info:'是否打印信息' = True,
-                    ranges:'数据范围'= {'C':[0, 1.2],
-                              'Si':[0, 4],
-                              'T3':[25, 450],
-                              'Y':[900, 2500],
-                              },
+                 columns: '选取的数据维度' = ['C', 'Si', 'Mn', 'Ni', 'Cr', 'Mo', 'Al', 'Co', '等温温度T2', '回火温度T3', '抗拉强度'],
+                 scaler: '是否采用数据缩放' = StandardScaler,
+                 regression: '用于分类还是回归' = True,
+                 shuffle: '' = True,
+                 random_seed: '' = 1,
+                 categories: '分类类别' = 4,
+                 onehot: '是否使用onehot' = False,
+                 info: '是否打印信息' = True,
+                 ranges: '数据范围' = {'C': [0, 1.2],
+                                   'Si': [0, 4],
+                                   'T3': [25, 450],
+                                   'Y': [900, 2500],
+                                   },
                  ):
         self.columns = columns
-        self.sclaer = scaler
+        self.scaler = scaler
         self.regression = regression
         self.shuffle = shuffle
         self.random_seed = random_seed
@@ -52,7 +46,7 @@ class feature_energing:
         对数据进行处理
         :return:
         """
-        if self.info:print('对数据进行回归处理' if self.regression else '对数据进行分类处理')
+        if self.info: print('对数据进行回归处理' if self.regression else '对数据进行分类处理')
 
         df = self.origin_df[self.columns]
         if self.info:
@@ -71,25 +65,25 @@ class feature_energing:
         # 强度处理
         df = df[df.Y >= self.ranges['Y'][0]]
         df = df[df.Y <= self.ranges['Y'][1]]
-        if self.info:print('--Del Y not in range:', np.shape(df))
+        if self.info: print('--Del Y not in range:', np.shape(df))
 
         # C处理 Nan的为不是数字的
         df = df[df.C.notnull()]
         df = df[df.C >= self.ranges['C'][0]]
         df = df[df.C <= self.ranges['C'][1]]
-        if self.info:print('--Del C not in range:', np.shape(df))
+        if self.info: print('--Del C not in range:', np.shape(df))
 
         #
         df = df[df.T2.notnull()]
         # df = df[df.T2 >= self.ranges['T2'][0]]
         # df = df[df.T2 <= self.ranges['T2'][1]]
-        if self.info:print('--Del T2 None', np.shape(df))
+        if self.info: print('--Del T2 None', np.shape(df))
 
         #
         df.loc[(df.T3 == 0), 'T3'] = 25
         df = df[df.T3 >= self.ranges['T3'][0]]
         df = df[df.T3 <= self.ranges['T3'][1]]
-        if self.info:print('--Del T3 not in range:', np.shape(df))
+        if self.info: print('--Del T3 not in range:', np.shape(df))
 
         # 其它元素位置 None填充0
         df = df.fillna(value=0)
@@ -101,9 +95,9 @@ class feature_energing:
         self.origin_range = pd.DataFrame([mins, maxs], index=['min:', 'max'], columns=self.columns)
         # if self.info: print('原始数据范围:', self.origin_range, sep='\n')
 
-        X = df.iloc[:, :-1].as_matrix()
-        y = df.Y.as_matrix()
-        if not self.regression:     # 非回归处理label
+        X = df.iloc[:, :-1].values
+        y = df.Y.values
+        if not self.regression:  # 非回归处理label
             y = self.deal_labels(y)
 
         self.target_df = df
@@ -114,17 +108,19 @@ class feature_energing:
                                                             random_state=self.random_seed)
 
         # 数据缩放
-        if not self.regression:
-            scaler = self.sclaer()
-            scaler = scaler.fit(X_train)
-            X_train = scaler.transform(X_train)
-            X_test = scaler.transform(X_test)
+        if self.regression:
+            s = MinMaxScaler().fit(X_train)
+            X_train = s.transform(X_train)
+            X_test = s.transform(X_test)
+
+            y_train = (y_train - 900) / (2500 - 900)
+            y_test = (y_test - 900) / (2500 - 900)
 
         # 各维度最大最小值
         mins = np.min(df, axis=0)
         maxs = np.max(df, axis=0)
         self.target_range = pd.DataFrame([mins, maxs], index=['min:', 'max'], columns=df.columns)
-        if self.info:print('处理后各维度范围:', self.target_range, sep='\n')
+        if self.info: print('处理后各维度范围:', self.target_range, sep='\n')
 
         self.X_train = X_train
         self.y_trian = y_train
@@ -217,16 +213,6 @@ def evaluate_regression(y, y_pred):
     print(f'RMSE:{int(np.sqrt(mean_squared_error(y, y_pred)))}')
     print('抽样随机结果对比：')
     index = np.random.randint(1, 100)
-    result = pd.DataFrame([y[index:index+5], y_pred[index:index+5]], index=['y', 'y_pred'])
+    result = pd.DataFrame([y[index:index + 5], y_pred[index:index + 5]], index=['y', 'y_pred'])
     # print(result)
     pass
-
-
-if __name__ == "__main__":
-    # path = r'C:\Users\chenshuai\Documents\材料学院\data\贝氏体钢数据统计-总20180421_pd.xlsx'
-    path = r'C:\Users\chenshuai\Documents\材料学院\data\贝氏体钢数据统计-总20180502.xlsx'
-
-    # 查看第三次仅仅增加数据的信息
-    fe = feature_energing(file=path, regression=True)
-    fe.origin_df = fe.origin_df.drop([x for x in range(1, 990)])
-    fe.preprocess()
