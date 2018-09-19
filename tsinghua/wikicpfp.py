37f0213e65f05d109318b7abee461831dc897fb0
nocater <nocater@163.com>
2018-09-19 11:26:25 +0800

    完成所有会议的基本信息抓取

--- tsinghua/wikicpfp.py
+++ tsinghua/wikicpfp.py
@@ -1,6 +1,7 @@
 # WikiCFP抓取
 import requests
 from lxml import etree
+import time
 
 class thConference:
     """
@@ -19,30 +20,29 @@ class Conference:
     def __init__(self, name:'会议全程'=None, abbr:'会议别称'=None, link:'链接地址'=None, past=None, future=None, event:list()=None):
         self.name = name
         self.abbr = abbr
+        self.link = link
         self.past = past
         self.future = future
         self.event = event
 
 
-
+domin = 'http://www.wikicfp.com/'
 urls = ['http://www.wikicfp.com/cfp/series?t=c&i='+chr(i) for i in range(65,65+26)]
-print(urls)
-for url in urls:
-    confs = []
 
+confs = []
+for url in urls:
     response = requests.get(url)
     page = etree.HTML(response.text)
     bgcolors = ['#f6f6f6', '#e6e6e6']
     for bgcolor in bgcolors:
-        a_s = page.xpath('//tr[@bgcolor="'+bgcolor+'"]/td/a/text()')
+        a_s = page.xpath('//tr[@bgcolor="'+bgcolor+'"]/td/a')
         tds = page.xpath('//tr[@bgcolor="'+bgcolor+'"]/td/text()')
-        a_s = [s.strip() for s in a_s if s != '\n']
+        # a_s = [s.strip() for s in a_s if s != '\n']
         tds = [s.replace('-','').strip() for s in tds if s != '\n']
         print(len(a_s),len(tds))
         for a,td in zip(a_s,tds):
-            confs.append(Conference(name=td, abbr=a))
+            confs.append(Conference(name=td, abbr=a.text, link=domin+a.attrib['href']))
 
-    [print(c.abbr, c.name, sep=':') for c in confs]
+    [print(c.abbr, c.name, c.link ,sep=':') for c in confs]
+    print('Doen',len(confs))
     break
-
-# 准备完成Conference.link
