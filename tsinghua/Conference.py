e4a36d7ac88d182745e58acab539bc729b793292
nocater <nocater@163.com>
2018-09-21 09:33:32 +0800

    添加id字段

--- tsinghua/Conference.py
+++ tsinghua/Conference.py
@@ -1,7 +1,8 @@
 class Conference:
-    def __init__(self, name:'会议全程'=None, abbr:'会议别称'=None,series:'是否在Series中'=False,
+    def __init__(self, id=None, name:'会议全程'=None, abbr:'会议别称'=None,series:'是否在Series中'=False,
                  link:'链接地址'=None, main_link:'会议主网址'=None,
                  past=None, future=None, event:list()=None):
+        self.id = id
         self.name = name
         self.abbr = abbr
         self.series = series
--- tsinghua/wikicpfp.py
+++ tsinghua/wikicpfp.py
@@ -8,94 +8,36 @@ from datetime import datetime
 from tsinghua.Conference import Conference
 from tsinghua.thConference import thConference
 
-domin = 'http://www.wikicfp.com/'
-urls = ['http://www.wikicfp.com/cfp/series?t=c&i='+chr(i) for i in range(65,65+26)]
 
-# 按字母表顺序抓取会议URL
-confs = []
-for url in urls[:1]:
-    response = requests.get(url)
-    page = etree.HTML(response.text)
-    bgcolors = ['#f6f6f6', '#e6e6e6']
-    for bgcolor in bgcolors:
-        a_s = page.xpath('//tr[@bgcolor="'+bgcolor+'"]/td/a')
-        tds = page.xpath('//tr[@bgcolor="'+bgcolor+'"]/td/text()')
-        # a_s = [s.strip() for s in a_s if s != '\n']
-        tds = [s.replace('-','').strip() for s in tds if s != '\n']
-        for a,td in zip(a_s,tds):
-            confs.append(Conference(name=td, abbr=a.text, link=domin+a.attrib['href']))
+def parseConference(conf: Conference):
 
-    # [print(c.abbr, c.name, c.link ,sep=':') for c in confs]
-    print('已经抓取', len(confs), '会议URL')
-    break
-
-# 对每个会议进行详细抓取
-for conf in confs[:5]:
     response = requests.get(conf.link)
-    page = etree.HTML(response.text)
 
+    # 1 抓取 past future
+    page = etree.HTML(response.text)
     past = page.xpath('//span[@class="theme"]/following-sibling::a/@href')
-    if past and len(past)==1: conf.past = past[0]
+    if past and len(past) == 1: conf.past = past[0]
     future = page.xpath('//following-sibling::span[@class="theme"]/following-sibling::span/a/text()')
-    if future and len(future)>0: conf.future = int(future[0].split(' ')[0])
+    if future and len(future) > 0: conf.future = int(future[0].split(' ')[0])
 
     print('抓取会议', conf.abbr, conf.link)
+
     # 2 抓取所有届会议信息
     events = []
     bgcolors = ['#f6f6f6', '#e6e6e6']
     for bgcolor in bgcolors:
         a_s = page.xpath('//tr[@bgcolor="' + bgcolor + '"]/td/a')
         for a in a_s:
-            events.append(thConference(abbr=a.text, name=conf.name, year=int(a.text.strip().split()[-1]),link=domin+a.attrib['href']))
+            events.append(thConference(abbr=a.text, name=conf.name, year=int(a.text.strip().split()[-1]),
+                                       link=domin + a.attrib['href']))
 
     # 2.1抓取届会议详细信息
     for event in events:
-        print("抓取届信息",event.year)
-        response = requests.get(event.link)
-        page = etree.HTML(response.text)
+        print('解析届会议', event.year)
+        event, series = parsethConf(event)
+        if series and conf.series is False: conf.series = series
 
-        thname = page.xpath('//span[@property="v:description"]/text()')
-        if thname and len(thname)==1: event.name = thname
-
-        series = page.xpath('//*/text()')
-        if 'Conference Series' in ''.join(series): conf.series =1
-
-        offilical_link = page.xpath('//td[contains(text(), "Link:")]/a/@href')
-        if len(offilical_link)==1: event.official_link = offilical_link[0]
-
-        when = page.xpath('//th[contains(text(),"When")]/following-sibling::td/text()')
-        if when and len(when)==1 and when[0].strip() != 'N/A':
-            when = when[0].strip()
-            stardate, enddate = [datetime.strptime(i.strip(), '%b %d, %Y') for i in when.split('-')]
-            event.stardate = datetime.strftime(stardate, '%Y-%m-%d')
-            event.enddate = datetime.strftime(enddate, '%Y-%m-%d')
-
-        where = page.xpath('//th[contains(text(),"Where")]/following-sibling::td/text()')
-        if where and len(where)==1 and where[0].strip() != 'N/A': event.where = where[0].strip()
-
-        submission_deadine = page.xpath(
-            '//th[contains(text(),"Submission")]/following-sibling::td//span/span[@property="v:startDate"]'
-            '/@content')
-        if submission_deadine and len(submission_deadine)==1 and submission_deadine[0].strip() != 'N/A':
-            event.submission_deadline = submission_deadine[0].strip().split('T')[0]
-
-        notification_due = page.xpath(
-            '//th[contains(text(),"Notification")]/following-sibling::td//span/span[@property="v:startDate"]'
-            '/@content')
-        if notification_due and len(notification_due)==1 and notification_due[0].strip() != 'N/A':
-            event.notification_due = notification_due[0].strip().split('T')[0]
-
-        final_version_due = page.xpath(
-            '//th[contains(text(),"Final")]/following-sibling::td//span/span[@property="v:startDate"]'
-            '/@content')
-        if final_version_due and len(final_version_due)==1 and final_version_due[0].strip() != 'N/A':
-            event.final_version_due = final_version_due[0].strip().split('T')[0]
-
-        categories = page.xpath('//a[@class="blackbold"]/following-sibling::a/text()')
-        event.categories = categories
-
-
-        # print(event.when, event.where, event.submission_deadline, event.notification_due, event.final_version_due, sep=" = ")
+        # print(event.stardate, event.submission_deadline, event.notification_due, event.final_version_due, sep=" = ")
         # time.sleep(1)
         # break # 只抓一届会议
 
@@ -105,14 +47,88 @@ for conf in confs[:5]:
 
     # 判断会议主网站是否存在
     offilical_links = [e.official_link.split('/')[2] for e in events if e.official_link]
-    if offilical_links and len(set(offilical_links))==1:
-        conf.main_link = 'http://'+offilical_links[0]
+    if offilical_links and len(set(offilical_links)) == 1:
+        conf.main_link = 'http://' + offilical_links[0]
 
-    # [print(e.year, e.abbr, e.name, e.link) for e in events]
-    # break #只抓一个会议
+    return conf
 
 
-# 将对象序列化 使用json
-data = json.dumps(confs, default=lambda obj: obj.__dict__)
-with open(r'D:\Documents\清华数据任务\data\cfp.json', 'w') as f:
-    f.write(data)
+def parsethConf(event: thConference=None):
+    SERIES = False
+    response = requests.get(event.link)
+    page = etree.HTML(response.text)
+
+    thname = page.xpath('//span[@property="v:description"]/text()')
+    if thname and len(thname) == 1: event.name = thname
+
+    series = page.xpath('//*/text()')
+    if 'Conference Series' in ''.join(series): SERIES = True
+
+    offilical_link = page.xpath('//td[contains(text(), "Link:")]/a/@href')
+    if len(offilical_link) == 1: event.official_link = offilical_link[0]
+
+    when = page.xpath('//th[contains(text(),"When")]/following-sibling::td/text()')
+    if when and len(when) == 1 and when[0].strip() != 'N/A':
+        when = when[0].strip()
+        stardate, enddate = [datetime.strptime(i.strip(), '%b %d, %Y') for i in when.split('-')]
+        event.stardate = datetime.strftime(stardate, '%Y-%m-%d')
+        event.enddate = datetime.strftime(enddate, '%Y-%m-%d')
+
+    where = page.xpath('//th[contains(text(),"Where")]/following-sibling::td/text()')
+    if where and len(where) == 1 and where[0].strip() != 'N/A': event.where = where[0].strip()
+
+    submission_deadine = page.xpath(
+        '//th[contains(text(),"Submission")]/following-sibling::td//span/span[@property="v:startDate"]'
+        '/@content')
+    if submission_deadine and len(submission_deadine) == 1 and submission_deadine[0].strip() != 'N/A':
+        event.submission_deadline = submission_deadine[0].strip().split('T')[0]
+
+    notification_due = page.xpath(
+        '//th[contains(text(),"Notification")]/following-sibling::td//span/span[@property="v:startDate"]'
+        '/@content')
+    if notification_due and len(notification_due) == 1 and notification_due[0].strip() != 'N/A':
+        event.notification_due = notification_due[0].strip().split('T')[0]
+
+    final_version_due = page.xpath(
+        '//th[contains(text(),"Final")]/following-sibling::td//span/span[@property="v:startDate"]'
+        '/@content')
+    if final_version_due and len(final_version_due) == 1 and final_version_due[0].strip() != 'N/A':
+        event.final_version_due = final_version_due[0].strip().split('T')[0]
+
+    categories = page.xpath('//a[@class="blackbold"]/following-sibling::a/text()')
+    event.categories = categories
+    return event, SERIES
+
+
+if __name__ == '__main__':
+    domin = 'http://www.wikicfp.com/'
+    urls = ['http://www.wikicfp.com/cfp/series?t=c&i='+chr(i) for i in range(65,65+26)]
+
+    # 按字母表顺序抓取会议URL
+    confs = []
+    for url in urls[:1]:
+        response = requests.get(url)
+        page = etree.HTML(response.text)
+        bgcolors = ['#f6f6f6', '#e6e6e6']
+        for bgcolor in bgcolors:
+            a_s = page.xpath('//tr[@bgcolor="'+bgcolor+'"]/td/a')
+            tds = page.xpath('//tr[@bgcolor="'+bgcolor+'"]/td/text()')
+            # a_s = [s.strip() for s in a_s if s != '\n']
+            tds = [s.replace('-','').strip() for s in tds if s != '\n']
+            for a,td in zip(a_s,tds):
+                confs.append(Conference(name=td, abbr=a.text, link=domin+a.attrib['href']))
+
+        # [print(c.abbr, c.name, c.link ,sep=':') for c in confs]
+        print('已经抓取', len(confs), '会议URL')
+        break
+
+    # 对每个会议进行详细抓取
+    for conf in confs[:5]:
+        parseConference(conf)
+        # [print(e.year, e.abbr, e.name, e.link) for e in events]
+        # break #只抓一个会议
+
+    # 将对象序列化 使用json
+    data = json.dumps(confs, default=lambda obj: obj.__dict__)
+    with open(r'D:\Documents\清华数据任务\data\cfp.json', 'w') as f:
+        f.write(data)
