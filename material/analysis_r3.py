d212dd896832f5bceb7fe7cb06212b4c367f74ff
nocater <nocater@163.com>
2019-04-01 18:26:21 +0800

    analysis T2-Y(T3=320 C=0.7)

--- material/analysis_r3.py
+++ material/analysis_r3.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
 df = pd.read_csv(r'D:\Documents\材料学院\data\贝氏体钢数据-fixedT3(320).csv')
 
@@ -11,3 +12,11 @@ df.loc[df.C == 0.71, 'C'] = 0.7
 # 对元素进行分组
 means = df.groupby(['C', 'Si', 'Mn', 'Ni', 'Cr', 'Mo', 'Al', 'T2', 'T3']).mean()
 print(means)
+
+# 分析结论： T3=320, C=0.7下 T2与Y负相关
+df = pd.read_csv(r'/mnt/d/Documents/材料学院/data/贝氏体钢数据-fixedT3(320).csv')
+df = df[df.T2 > 0]
+df = df[df.C == 0.7]
+
+plt.scatter(df.T2, df.Y)
+plt.show()
