45f0b6386c66aa1684dbe269cf613e926a5e4b3e
nocater <nocater@163.com>
2019-05-18 17:13:23 +0800

    add Ys

--- material/datahelper.py
+++ material/datahelper.py
@@ -34,7 +34,8 @@ class feature_energing:
                                    'T3': [25, 450],
                                    'Y': [900, 2500],
                                    },
-                 rename: '列名更改' = {'等温温度T2': 'T2', '回火温度T3': 'T3', '抗拉强度': 'Y'}
+                 dic_rename: '列名更改' = {'等温温度T2': 'T2', '回火温度T3': 'T3', '抗拉强度': 'Y'},
+                 Ys=1,
                  ):
         self.columns = columns
         self.scaler = scaler
@@ -47,8 +48,8 @@ class feature_energing:
         self.ranges = ranges
         self.origin_df = pd.read_excel(file, header=2)
         self.origin_df = self.origin_df[self.origin_df.index.notnull()]
-        self.rename = rename
-        pass
+        self.dic_rename = dic_rename
+        self.Ys = Ys
 
     def preprocess(self):
         """
@@ -63,7 +64,7 @@ class feature_energing:
             df.info()
 
         # 重命名列名
-        df = df.rename(columns=self.rename)
+        df = df.rename(columns=self.dic_rename)
 
         ## 数据处理
         df = df.fillna(value=0)
@@ -104,7 +105,7 @@ class feature_energing:
         self.origin_range = pd.DataFrame([mins, maxs], index=['min:', 'max'], columns=self.columns)
         # if self.info: print('原始数据范围:', self.origin_range, sep='\n')
 
-        X = df.iloc[:, :-1].values
+        X = df.iloc[:, :-self.Ys].values
         y = df.Y.values
         if not self.regression:  # 非回归处理label
             y = self.deal_labels(y)
