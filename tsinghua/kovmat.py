fe2e4df82a45d4bd0ccc84d08c9d2a1aceb25fbf
nocater <nocater@163.com>
2019-12-05 18:24:42 +0800

    done

--- tsinghua/kovmat.py
+++ tsinghua/kovmat.py
@@ -23,7 +23,7 @@ issn = '0023-432X'
 data_file = './data/kovmat/'
 
 
-# In[3]:
+# In[11]:
 
 
 def parse(tree, rr, cc):
@@ -63,6 +63,7 @@ def parse(tree, rr, cc):
 
     # [print(len(x)) for x in [titles,authors,keywords,dois,vols,nos,page_start,page_end,pps]]
 
+    [print(i) for i in page_start]
     pdfs = [list([f'http://www.kovmat.sav.sk/full.php?rr={rr}&cc={cc}&ss={page_start[i]}']) for i in range(len(titles))]
 
     urls = [list([link[0].replace('full', 'abstract')]) for link in pdfs]
@@ -174,12 +175,12 @@ def save(data, file):
 # save(save_data, data_file+'kovmat_1995-2019.json')
 
 
-# In[ ]:
+# In[12]:
 
 
 if __name__ == '__main__':
     all_pubs = []
-    for rr in range(33, 59):
+    for rr in range(58, 59):
         vol_pubs = []
         for cc in range(1, 7):
             url = f'http://www.kovmat.sav.sk/issue.php?rr={rr}&cc={cc}'
@@ -211,5 +212,128 @@ if __name__ == '__main__':
         all_pubs.extend(vol_pubs)
 
 print('All spidered!')
-save(all_pubs, f'{data_file}kovmat_all.json')
+save(all_pubs,  f'{data_file}kovmat_all.json')
+
+
+# # Parse Lasted
+
+# In[48]:
+
+
+def parse_without_pages(tree, rr, cc):
+    titles = tree.xpath("//table[@width='95%']//p/font[@size='+1']")
+    t = tree.xpath("//table[@width='95%']//td/font[@size='+1']")
+    titles.extend(t)
+    raw_titles = [etree.tostring(i).decode('utf8').strip() for i in titles]
+    titles = [etree.tostring(i).decode('utf8')[19:-11].strip() for i in titles]
+    titles
+
+    authors_keywords = tree.xpath("//table[@width='95%']//p/font[@size='+0']")
+    ak = tree.xpath("//table[@width='95%']//td/font[@size='+0']")
+    authors_keywords.extend(ak)
+    authors = [etree.tostring(i).decode('utf8')[19:-11].strip() for i in authors_keywords[::2]]
+    keywords = [etree.tostring(i).decode('utf8')[34:-15].strip() for i in authors_keywords[1::2]]
+    keywords
+
+    dois = tree.xpath('//table//p/text()[2]')
+    d = tree.xpath('//table//td/text()')[13]
+    dois.append(d)
+    dois = [i.split('DOI:')[1].strip() for i in dois if 'DOI' in i]
+    dois
+
+    vnps = tree.xpath('//table//p/text()[1]')
+    v = tree.xpath('//table//td/text()')[12]  # diff
+    vnps.append(v)
+    vols = [i.split(',')[0].split('vol.')[1].strip() for i in vnps if 'vol' in i]
+    vols = [i.split()[0] for i in vols]
+    nos = [i.split(',')[1].split('no.')[1].strip() for i in vnps if 'no' in i]
+    pps = [i.split(',')[2].split('pp.')[1].strip() for i in vnps if 'pp' in i]
+    pps = [i.replace(' ', '') for i in pps]
+    vols, nos, pps
+
+    abs_urls = tree.xpath('//a[contains(@href,"abstract")]/@href')
+    urls = [list([base_url + '/' + i]) for i in abs_urls]
+    abstracts = [parse_abstract(u) for u in urls]
+
+    dois = dois if len(dois) == len(titles) else [""] * len(titles)
+    assert len(titles) == len(authors) == len(keywords) == len(dois)
+
+    return urls, raw_titles, titles, abstracts, authors, keywords, dois, vols, nos
+
+
+# In[51]:
+
+
+def generate_without_pages(infos, year, venue, src='kovove', issn='0023-432X'):
+    def foo_title(x):
+        x = x.replace('<sub>', '')
+        x = x.replace('</sub>', '')
+        return x
+
+    def foo_author(author):
+        a_list = []
+        for i, name in enumerate(author.split('.,')):
+            a = dict()
+            a['name'] = name.strip()
+            a['pos'] = i
+            a['sid'] = str(hash(name))
+            a_list.append(a)
+        return a_list
+
+    def foo_hash(title):
+        return ''.join([i[0].lower() for i in title.split()])
+
+    pubs = []
+    for url, raw_title, title, abstract, author, keyword, doi, vol, no in zip(*infos):
+        pub = dict()
+        pub.update(zip(
+            ['url', 'raw_title', 'title', 'abstract', 'authors', 'venue', 'keywords', 'volume', 'issue', 'hash', 'ts',
+             'src', 'lang', 'sid', 'year', 'issn'],
+            [url, raw_title, foo_title(title), abstract, foo_author(author), venue,
+             ';'.join([i.strip() for i in keyword.split(',')]), vol, no, foo_hash(title), str(now()), src, 'en',
+             str(hash(title)), year,
+             issn]))
+
+        if doi != '' or doi.strip() != '':
+            pub['doi'] = doi.strip()
+        #         print(repr(pub))
+        #         print('--------------')
+        pubs.append(pub)
+
+    return pubs
+
+
+# response = requests.get('http://www.kovmat.sav.sk/issue.php?rr=33&cc=1')
+# tree = etree.HTML(response.text)
+# r = parse(tree, 33, 1)
+# venue = {'name':'Kovove Materialy - Metallic Materials', 'type':1, 'sid':'1'}
+# r = generate(r, 2019, venue)
+
+
+# In[52]:
+
+
+rr = 58
+cc = 1
+url = f'http://www.kovmat.sav.sk/issue.php?rr={rr}&cc={cc}'
+tree = etree.HTML(response.text)
+# 0 records
+records = int(tree.xpath('.//i[contains(text(),"records ")]/text()')[0].split()[1])
+print(f'found  records: {records} ')
+inofs = parse_without_pages(tree, rr, cc)
+pubs = generate_without_pages(inofs, year, venue)
+print(f'spider success: {len(pubs)}')
+save(pubs, f'{data_file}kovmat_2020.json')
+
+# In[53]:
+
+
+all_data = []
+for year in range(1995, 2021):
+    with open(f'{data_file}kovmat_{year}.json') as f:
+        data = eval(''.join(f.readlines()))
+        all_data.extend(data)
+
+print(len(all_data))
+save(all_data, f'{data_file}kovmat_all.json')
 
